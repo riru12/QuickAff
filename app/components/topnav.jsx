283bd33596ffdbc60834d99a451ecbf6fcf43a82
riru12 <brilljulianrina@gmail.com>
2023-11-10 16:26:35 +0800

    top navbar and side navbar implemented

---
+++ app/components/topnav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TopNav = () => {
+    return (
+        <div className="w-screen absolute z-10 bg-[#858585] left-0 top-0 p-4 flex justify-between">
+            <div className="font-bold">
+                UP CURSOR
+            </div>
+            <div>
+                User
+            </div>
+        </div>
+    )
+}
+
+export default TopNav
