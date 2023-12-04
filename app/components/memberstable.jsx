c7bfcc669fa8c0876049592080c90d00137ac6da
riru12 <brilljulianrina@gmail.com>
2023-12-04 21:29:44 +0800

    working basic table

--- app/components/memberstable.jsx
+++ app/components/memberstable.jsx
@@ -1,260 +1,44 @@
-import React from 'react';
+"use client"
+import { React,useEffect, useState } from 'react';
+import data from './users.json';
 
-const fieldArray = ["Student ID", "Last Name", "First Name", "Middle Name", "Nickname", "Course", "UP Mail", "App Batch", "Membership Status", "Renewal Payment Status", "Committee", "Team/s"]
+const fieldArray = ["Student ID", "Last Name", "First Name", "Middle Name", "Nickname", "UP Mail", "Course", "App Batch", "Membership Status", "Renewal Payment", "Committee"]
 const tableFields = fieldArray.map((field, index)=>(
     <th className="p-2" key={index}>
         {field}
     </th>
 ))
 
+var tableData = data.map((entry,index) => (
+    <tr>
+        <td className="pl-2 pr-2">{entry.id}</td>
+        <td className="pl-2 pr-2">{entry.last_name}</td>
+        <td className="pl-2 pr-2">{entry.first_name}</td>
+        <td className="pl-2 pr-2">{entry.middle_name}</td>
+        <td className="pl-2 pr-2">{entry.nickname}</td>
+        <td className="pl-2 pr-2">{entry.up_mail}</td>
+        <td className="pl-2 pr-2">{entry.course}</td>
+        <td className="pl-2 pr-2">{entry.app_batch}</td>
+        <td className="pl-2 pr-2">{entry.mem_status}</td>
+        <td className="pl-2 pr-2">{entry.renewal_payment_status}</td>
+        <td className="pl-2 pr-2">{entry.committee}</td>
+    </tr>
+))
+
 const MembersTable = () => {
+    data.map((entry,index) => { 
+        console.log(`${entry.id}`)
+    })
     return (
-        <div className="text-black"> 
-            <table className="table-fixed h-52">
+        <div className="text-black ml-28"> 
+            <table className="table-fixed">
                 <thead id="table fields" className="border-b bg-neutral-600 font-medium text-white">
                     <tr>
                         {tableFields}
                     </tr>
                 </thead>
                 <tbody id="table content">
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
-                    <tr>
-                        <td className="pl-2 pr-2">202000000</td>
-                        <td className="pl-2 pr-2">Gonzaga</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">Panget</td>
-                        <td className="pl-2 pr-2">Chase</td>
-                        <td className="pl-2 pr-2">BS Computer Science</td>
-                        <td className="pl-2 pr-2">rcgonzaga2@up.edu.ph</td>
-                        <td className="pl-2 pr-2">IDKApp</td>
-                        <td className="pl-2 pr-2">Regular</td>
-                        <td className="pl-2 pr-2">Paid</td>
-                        <td className="pl-2 pr-2">Membership</td>
-                        <td className="pl-2 pr-2">Everything</td>
-                    </tr>
+                   {tableData}
                 </tbody>
                 </table>
         </div>
