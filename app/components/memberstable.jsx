7bfffb93ac7533b9c6b253d1fd01c786d4f61d23
riru12 <brilljulianrina@gmail.com>
2023-12-06 10:44:01 +0800

    fixed some table css

--- app/components/memberstable.jsx
+++ app/components/memberstable.jsx
@@ -6,7 +6,7 @@ function MembersTable() {
     const [data, setData] = useState(null)
     const [isLoading, setLoading] = useState(true)
 
-    const fieldArray = ["Student ID", "Last Name", "First Name", "Middle Name", "Nickname", "Course", "UP Mail", "App Batch", "Membership Status", "Renewal Payment Status", "Committee"]
+    const fieldArray = ["Student ID", "Last Name", "First Name", "Middle Name", "Nickname", "UP Mail", "Course", "App Batch", "Membership Status", "Renewal Payment Status", "Committee"]
     const tableFields = fieldArray.map((field, index)=>(
         <th className="p-2" key={index}>
             {field}
@@ -27,33 +27,34 @@ function MembersTable() {
     console.log(data)
 
     var tableData = data.map((entry,index) => (
-        <tr>
-            <td className="pl-2 pr-2">{entry.student_id}</td>
-            <td className="pl-2 pr-2">{entry.last_name}</td>
-            <td className="pl-2 pr-2">{entry.first_name}</td>
-            <td className="pl-2 pr-2">{entry.middle_name}</td>
-            <td className="pl-2 pr-2">{entry.nickname}</td>
-            <td className="pl-2 pr-2">{entry.up_mail}</td>
-            <td className="pl-2 pr-2">{entry.course}</td>
-            <td className="pl-2 pr-2">{entry.app_batch}</td>
-            <td className="pl-2 pr-2">{entry.mem_status}</td>
-            <td className="pl-2 pr-2">{entry.renewal_payment_status}</td>
-            <td className="pl-2 pr-2">{entry.committee}</td>
+        <tr class="bg-white border-b text-sm">
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.student_id}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.last_name}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.first_name}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.middle_name}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.nickname}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.up_mail}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.course}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.app_batch}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.mem_status}</td>
+            <td className="px-6 py-4 overflow-hidden whitespace-nowrap overflow-x-auto">{entry.renewal_payment_status}</td>
+            <td className="px-6 py-4">{entry.committee}</td>
         </tr>
     ))
 
     return (
-        <div className="text-black ml-40 mr-2 mt-16"> 
-            <table className="table-fixed">
-                <thead id="table fields" className="border-b bg-neutral-600 font-medium text-white">
-                    <tr>
-                        {tableFields}
-                    </tr>
-                </thead>
-                <tbody id="table content">
-                   {tableData}
-                </tbody>
-                </table>
+        <div className="ml-40 mt-16 h-full flex">
+            <div className="flex flex-col">
+                <input class="rounded w-48 shadow py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Search"/>
+                <div className="text-black overflow-auto shadow-md sm:rounded-lg w-full max-h-[90vh]"> {/* Adjust max height as needed */}
+                    <table className="table-fixed w-full">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>{tableFields}</tr>
+                        </thead>
+                        <tbody id="table content">{tableData}</tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
--- app/components/renewal.jsx
+++ app/components/renewal.jsx
@@ -18,14 +18,14 @@ const Renewal = () => {
     // PAST DREI: Di ako sure bat kahit na may left margin na sa parent
     // div (ml-32), di sumasama mga anak nya kaya may ml-32 pa sa button
     // na Begin Renewal for instance
-    <div className="text-black ml-32 flex"> 
+    <div className="text-black ml-32 p-4 flex"> 
       <button
-        className="bg-red-500 px-4 ml-32 text-white pt-0 rounded font-bold text-3xl"
+        className="bg-red-500 px-4 text-white pt-0 rounded font-bold text-3xl"
         onClick={handleRenewalClick}
       >
         Begin Renewal
       </button>
-      <div className="mt-4 ml-4 max-w-3xl"> 
+      <div className="p-4 max-w-3xl"> 
         <p className="text-red-600 font-bold ">
           Warning
         </p>
